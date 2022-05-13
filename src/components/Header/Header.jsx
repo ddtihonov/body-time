474bc9dea18b587c945467e061446ed204a4ea75
Dmitry <ddtihonov@yandex.ru>
2022-05-13 16:41:24 +0300

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import header from './header.module.css';
+import header from './header.module.scss';
 import logo from '../../image/logo.svg'
 import WhatsApp from '../../image/WhatsApp.svg'
 import tel from '../../image/tel.svg'
@@ -9,9 +9,12 @@ export default function Header() {
     return (
       <header className={header.header}>
         <div className={header.box}>
+          <div className={header.menu__icon}>
+              <span></span>
+          </div>
           <img className={header.logo} src={logo} alt = 'логотип'/>
           <nav className={header.nav}>
-            <a className={header.link} href='#'>Наши услуги</a>
+            <a className={header.link} href='#'>Услуги</a>
             <a className={header.link} href='#'>Акции</a>
             <a className={header.link} href='#'>Прайс</a>
             <a className={header.link} href='#'>Контакты</a>
@@ -25,6 +28,7 @@ export default function Header() {
             <img className={header.image} src={location} alt = 'логотип локация'/>
             <p className={header.location_text}>г. Новосибирск, ул. Вокзальная магистраль,16, оф. 205, 2 эт.</p>
           </div>
+          <a className={header.link_price} href='#'>Прайс</a>
         </div>
     
       </header>
