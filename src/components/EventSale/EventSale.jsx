d393614ce359db6676a2938030f4ca45db76ac33
Dmitry <ddtihonov@yandex.ru>
2022-06-29 15:15:09 +0300

    горизонтальный слайдер

--- src/components/EventSale/EventSale.jsx
+++ src/components/EventSale/EventSale.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, {useEffect, useState} from "react"
 import styles from './EventSale.module.scss'
-import SaleCard from '../SaleCard/SaleCard';
+import SaleCard from '../SaleCard/SaleCard'
 import {arrEventSale} from '../../utils/data'
+import { v4 as uuidv4 } from 'uuid'
+
 
 export default function EventSale() {
+    
+const [activeIndex, setActiveIndex] = useState(0)
+const [paused, setPaused] = useState(false)
 
-    const arr = arrEventSale[4]
+/*useEffect(() => {
+        const interval = setInterval (() => {
+            if (!paused){
+            updateIndex(activeIndex + 1)
+            }
+        }, 3000) 
+return () => {
+    if (interval) {
+        clearInterval(interval)
+    }
+}
+})*/
+
+
+const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+        newIndex = 4
+    } else if (newIndex > 4) {
+        newIndex = 0
+    }
+
+    setActiveIndex(newIndex)
+}
+
 
     return (
-        <section className={styles.main}>
+        <section 
+        className={styles.main}
+        onMouseEnter={() => setPaused(true)}
+        onMouseLeave={() => setPaused(false)}
+        >
             <h2 className={styles.title}>Акции</h2>
-            <SaleCard
-                title={arr.title}
-                text={arr.text}
-                price={arr.price}
-                image={arr.image.image_5}
-            />
-            <div className={styles.box}>
-                <button className={styles.button_left}></button>
-                <button className={styles.button_right}></button>
+            <div className={styles.slides_box}>
+                <ul className={styles.slides} style ={{transform: `translateX(-${activeIndex * 100}%)`}}>
+                {arrEventSale.map((item, index) => {
+                    const keyUid = uuidv4();
+                    return (
+                        <SaleCard
+                            title={item.title}
+                            text={item.text}
+                            price={item.price}
+                            image={item.image}
+                            key={keyUid}
+                        />
+                    )
+                    })}
+                </ul>
             </div>
-
+            <div className={styles.box}>
+                <button className={styles.button_left} type="button" onClick={() => updateIndex(activeIndex - 1)}/>
+                <ul className={styles.list}>
+                    {arrEventSale.map((item, index) => {
+                        const keyUid = uuidv4();
+                        return (
+                            <li className={styles.item} key={keyUid}>
+                                <button className={`${activeIndex === index ? styles.button_active : styles.button}`} type="button" onClick={() => updateIndex(index)}/>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <button className={styles.button_right} type="button" onClick={() => updateIndex(activeIndex + 1)}/>
+            </div>
         </section>
     )
     
--- src/components/SaleCard/SaleCard.jsx
+++ src/components/SaleCard/SaleCard.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import styles from './SaleCard.module.scss'
+import styles from './SaleCard.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export default function SaleCard({title, text, price, image}) {
 
     return (
-            <div className={styles.box}>
+            <li className={styles.box}>
                 <img className={styles.image} src={image} alt={title} />
                 <div className={styles.box_info}>
                     <h3 className={styles.title}>{title}</h3>
-                    {text.map((item, index) =>{
+                    {text.map(item =>{
+                            const keyUid = uuidv4();
                         return(
-                            <p className={styles.text} key={index}>{item}</p>
+                            <p className={styles.text} key={keyUid}>{item}</p>
                         )
                     })}
                     <p className={styles.prise}>{price}</p>
                 </div>
-            </div>
+            </li>
     )
     
 }
